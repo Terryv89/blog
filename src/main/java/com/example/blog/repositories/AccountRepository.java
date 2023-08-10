2fbbaaae011225fe3cc72fd20921bb88e74c942b
Terry vanker <Terry.vanker89@gmail.com>
2023-08-10 13:33:04 +0200

    .

--- src/main/java/com/example/blog/repositories/AccountRepository.java
+++ src/main/java/com/example/blog/repositories/AccountRepository.java
@@ -1,8 +1,10 @@
 package com.example.blog.repositories;
 
+import com.example.blog.models.Account;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public class AccountRepository {
+public interface AccountRepository extends JpaRepository<Account, Long> {
 
 }
